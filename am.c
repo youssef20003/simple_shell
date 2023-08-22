020bb46ee0500f1723d3af679495bff8185b8385
youssef20003 <ym05914@gmail.com.com>
2023-08-22 17:55:55 +0300

    a

---
+++ am.c
@@ -0,0 +1,105 @@
+#include "shell.h"
+
+/**
+ * set_alias - add, or override alias
+ * @alias_string: alias to be seted in the form (name='value')
+ * @data: struct for the program's data
+ * Return: zero if sucess, or other number if its declared in the arguments
+ */
+int set_alias(char *alias_string, data_of_program *data)
+{
+	int i, j;
+	char buffer[250] = {'0'}, *temp = NULL;
+
+	if (alias_string == NULL ||  data->alias_list == NULL)
+		return (1);
+	for (i = 0; alias_string[i]; i++)
+		if (alias_string[i] != '=')
+			buffer[i] = alias_string[i];
+		else
+		{
+			temp = get_alias(data, alias_string + i + 1);
+			break;
+		}
+	for (j = 0; data->alias_list[j]; j++)
+		if (str_compare(buffer, data->alias_list[j], i) &&
+			data->alias_list[j][i] == '=')
+		{
+			free(data->alias_list[j]);
+			break;
+		}
+
+	if (temp)
+	{
+		buffer_add(buffer, "=");
+		buffer_add(buffer, temp);
+		data->alias_list[j] = str_duplicate(buffer);
+	}
+	else
+		data->alias_list[j] = str_duplicate(alias_string);
+	return (0);
+}
+/**
+ * get_alias - add, remove or show aliases
+ * @data: struct for the program's data
+ * @name: name of the requested alias.
+ * Return: zero if sucess, or other number if its declared in the arguments
+ */
+char *get_alias(data_of_program *data, char *name)
+{
+	int i, alias_length;
+
+	if (name == NULL || data->alias_list == NULL)
+		return (NULL);
+
+	alias_length = str_length(name);
+
+	for (i = 0; data->alias_list[i]; i++)
+	{
+		if (str_compare(name, data->alias_list[i], alias_length) &&
+			data->alias_list[i][alias_length] == '=')
+			{
+			return (data->alias_list[i] + alias_length + 1);
+			}
+	}
+
+	return (NULL);
+
+}
+/**
+ * print_alias - add, remove or show aliases
+ * @data: struct for the program's data
+ * @alias: name of the alias to be printed
+ * Return: zero if sucess, or other number if its declared in the arguments
+ */
+int print_alias(data_of_program *data, char *alias)
+{
+	int i, j, alias_length;
+	char buffer[250] = {'\0'};
+
+	if (data->alias_list)
+	{
+		alias_length = str_length(alias);
+		for (i = 0; data->alias_list[i]; i++)
+		{
+			if (!alias || (str_compare(data->alias_list[i], alias, alias_length)
+				&&	data->alias_list[i][alias_length] == '='))
+			{
+				for (j = 0; data->alias_list[i][j]; j++)
+				{
+					buffer[j] = data->alias_list[i][j];
+					if (data->alias_list[i][j] == '=')
+						break;
+				}
+				buffer[j + 1] = '\0';
+				buffer_add(buffer, "'");
+				buffer_add(buffer, data->alias_list[i] + j + 1);
+				buffer_add(buffer, "'\n");
+				_print(buffer);
+			}
+		}
+	}
+
+	return (0);
+}
+
---
+++ execute.c
@@ -0,0 +1,45 @@
+#include "shell.h"
+/**
+ * execute - execute a command with its entire path variables.
+ * @data: a pointer to the program's data
+ * Return: If sucess returns zero, otherwise, return -1.
+ */
+int execute(data_of_program *data)
+{
+	int retval = 0, status;
+	pid_t pidd;
+
+	retval = builtins_list(data);
+	if (retval != -1)
+		return (retval);
+
+	retval = find_program(data);
+	if (retval)
+	{
+		return (retval);
+	}
+	else
+	{
+		pidd = fork();
+		if (pidd == -1)
+		{
+			perror(data->command_name);
+			exit(EXIT_FAILURE);
+		}
+		if (pidd == 0)
+		{
+			retval = execve(data->tokens[0], data->tokens, data->env);
+			if (retval == -1)
+				perror(data->command_name), exit(EXIT_FAILURE);
+		}
+		else
+		{
+			wait(&status);
+			if (WIFEXITED(status))
+				errno = WEXITSTATUS(status);
+			else if (WIFSIGNALED(status))
+				errno = 128 + WTERMSIG(status);
+		}
+	}
+	return (0);
+}
