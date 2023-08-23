78ee6f344c797a62e6a77afbd5c7920c07ad7e23
youssef20003 <ym05914@gmail.com>
2023-08-23 14:31:52 +0300

    a

--- built_ins2.c
+++ built_ins2.c
@@ -72,43 +72,6 @@ int set_work_directory(data_of_program *data, char *new_dir)
 	return (0);
 }
 /**
- * builtin_cd - change the current directory
- * @data: struct for the program's data
- * Return: zero if sucess, or other number if its declared in the arguments
- */
-int builtin_cd(data_of_program *data)
-{
-	char *dir_home = env_get_key("HOME", data), *dir_old = NULL;
-	char old_dir[128] = {0};
-	int error_code = 0;
-
-	if (data->tokens[1])
-	{
-		if (str_compare(data->tokens[1], "-", 0))
-		{
-			dir_old = env_get_key("OLDPWD", data);
-			if (dir_old)
-				error_code = set_work_directory(data, dir_old);
-			_print(env_get_key("PWD", data));
-			_print("\n");
-
-			return (error_code);
-		}
-		else
-		{
-			return (set_work_directory(data, data->tokens[1]));
-		}
-	}
-	else
-	{
-		if (!dir_home)
-			dir_home = getcwd(old_dir, 128);
-
-		return (set_work_directory(data, dir_home));
-	}
-	return (0);
-}
-/**
  * builtins_list - search for match and execute the associate builtin
  * @data: struct for the program's data
  * Return: Returns the return of the function executed is there is a match,
